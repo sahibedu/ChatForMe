a80f9989a3bcc24782aa16178985984332353795
Sahib <sahib1746.cse16@chitkara.edu.in>
2018-04-01 06:27:01 +0530

    commit 1.0 v2
    Added Functionality of Sending and Retreving Messages From Firebase,Added Struct For better Usage

---
+++ ChatMe/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ChatMe
+//
+//  Created by Sultan on 01/04/18.
+//  Copyright © 2018 Sultan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct messageFields {
+        static let username = "name"
+        static let text = "text"
+        static let imageURL = "imageurl"
+        
+    }
+}
--- ChatMe/MainViewController.swift
+++ ChatMe/MainViewController.swift
@@ -11,27 +11,62 @@ import Firebase
 
 class MainViewController: UIViewController {
     @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var messageTableView: UITableView!
     
     var dbReference : DatabaseReference!
+    var dbHandler : DatabaseHandle!
+    var messages : [DataSnapshot]! = []
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         databaseSetUp()
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
     }
     
     func databaseSetUp(){
         dbReference = Database.database().reference()
-        print("Database Referenced")
+        dbHandler = dbReference.child("messages").observe(.childAdded){(snapshot : DataSnapshot) in
+            self.messages.append(snapshot)
+            self.messageTableView.insertRows(at: [IndexPath(row: self.messages.count-1,section: 0)], with: .automatic)
+            self.messageTableView.reloadData()
+        }
     }
     
+    deinit {
+        dbReference.child("messages").removeObserver(withHandle: dbHandler)
+    }
+    //--------------------------------------------------------------------------------------------------------
+    //MARK: ACTIONS
+    //--------------------------------------------------------------------------------------------------------
+    
     @IBAction func CameraAction(_ sender: Any) {
+        print("Camera Button Called")
     }
     
     @IBAction func SendMsgAction(_ sender: Any) {
-        print(messageTextField.text!)
-        let messageText = messageTextField.text!
+        let data = [Constants.messageFields.username : "Optimus Prime",Constants.messageFields.text : messageTextField.text!]
         messageTextField.text = ""
         messageTextField.resignFirstResponder()
-        dbReference.child("messages").childByAutoId().setValue(messageText)
+        dbReference.child("messages").childByAutoId().setValue(data)
     }
+}
+
+
+extension MainViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let prototypeCell = messageTableView.dequeueReusableCell(withIdentifier: "messageBox", for: indexPath)
+        let messageSnapshot : DataSnapshot = messages[indexPath.row]
+        let message = messageSnapshot.value as! [String:String]
+        prototypeCell.textLabel?.text = message[Constants.messageFields.username]! + ": " + message[Constants.messageFields.text]!
+        return prototypeCell
+    }
+    
+    
 }
